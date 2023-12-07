8b38364c1ff1c92cc9df7aa5abbb8f5e94334ed7
Eddie Ayala <heriberto.ayala1981@gmail.com>
2023-12-07 16:42:15 -0600

    recurtion

---
+++ src/JavaMethods.java
@@ -0,0 +1,17 @@
+//public class JavaMethods {
+//   public static void main(String[] args){
+//       String myFirstName = "Eddie";
+//       String myLastName = "Ayala";
+//       String fullName = putNamesTogther(myFirstName, myLastName);
+//       System.out.println(fullName);
+//       putNamesTogther(myFirstName, myLastName);
+//       putNamesTogther(myFirstName);
+//   }
+//   public static String putNamesTogther(String firstname, String lastName) {
+//    String fullName = firstname + " " + lastName;
+//    return fullName;
+//   }
+//    public static String putNamesTogther(String firstname, ) {
+//
+//    }
+//}
